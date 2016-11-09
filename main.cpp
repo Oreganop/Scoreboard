c843fd8ef3b970a70dcbd6d01e111372a57a986f
Garrett Swann <Garrettswann3049@yahoo.com>
2016-11-09 12:16:32 -0800

    generalboard code working fine. Fixed driver code.

--- main.cpp
+++ main.cpp
@@ -6,9 +6,9 @@ using namespace std;
 int main()
 {
     generalboard b(5,5);
-    for (int i=0; i<5; i++)
+    for (int i=1; i<=5; i++)
     {
-        for (int j=0; j<5; j++)
+        for (int j=1; j<=5; j++)
         {
             cout << i+j;
             b.setScore(i,j,i+j);
